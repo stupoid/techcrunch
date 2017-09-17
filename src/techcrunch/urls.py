ef0ffdcfebe47c9c61f6c7fc9a36472a5ccac2ad
kelvin <there.are.no.mails@gmail.com>
2017-09-16 19:50:19 -0700

    Registration views and data works

--- src/techcrunch/urls.py
+++ src/techcrunch/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import url
-
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-    url(r'^index/$', views.index, name='index'),
+    url(r'^login', auth_views.login, {'template_name': 'techcrunch/login.html'}, name='login'),
+    url(r'^logout/$', auth_views.logout, name='logout'),
+    url(r'^signup', views.signup, name='signup'),
     url(r'^map/$', views.map, name='map'),
+    url(r'^', views.index, name='index'),
 ]
--- src/techcrunch/views.py
+++ src/techcrunch/views.py
@@ -1,7 +1,23 @@
-from django.shortcuts import render
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
 
 def index(request):
     return render(request, 'techcrunch/index.html')
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('techcrunch/index')
+    else:
+        form = UserCreationForm()
+    return render(request, 'techcrunch/signup.html', {'form': form})
 
 def map(request):
     return render(request, 'techcrunch/map.html')
