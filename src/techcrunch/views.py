306bfd5711d92f347f18bbfe22c2836533bd1102
l0g1cal <yongjiawern@gmail.com>
2017-09-16 18:43:27 -0700

    Fix models and include map in views

--- src/techcrunch/views.py
+++ src/techcrunch/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'techcrunch/index.html')
+
+def map(request):
+    return render(request, 'techcrunch/map.html')
